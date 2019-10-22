c996b43f12b2c70f001564bf1d8002ccc79adecc
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-10-22 21:04:36 +0100

    Create Footer

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = styled.div`
+  margin-right: 10px;
+  margin-left: auto;
+  margin-top: 20px;
+  font-size: 12px;
+`;
+
+export default Footer;
