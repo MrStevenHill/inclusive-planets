8397e5cc8cf42e88d69677e0cd6e1759971e8dae
Steven Hill <15816322+MrStevenHill@users.noreply.github.com>
2019-10-22 21:05:02 +0100

    Create Planet

---
+++ src/Planet.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const PlanetContainer = styled.div``;
+
+const Planet = ({ name }) => <PlanetContainer>{name}</PlanetContainer>;
+
+export default Planet;
